6abb615b616b065beedfa60ee51c8d538c4d1cb7
FlackCode <fl4ckc0de@gmail.com>
2024-11-09 21:26:09 +0200

    Created base layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,33 @@
 "use client"
 
-import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs";
 import { api } from "~/trpc/react";
 
 export default function Home() {
-  const { data } = api.post.getAll.useQuery();
+  const { data, isLoading } = api.post.getAll.useQuery();
+  if (isLoading) return <div>Loading...</div>
+  if (!data) return <div>Something went wrong...</div>
 
   return (
-    <div>
-      <h1>Hello World</h1>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <SignOutButton />
-        <UserButton />
-      </SignedIn>
-      <div>
-        {data?.map((post) => (
-          <div key={post.id}>{post.content}</div>
-        ))}
+    <div className="flex justify-center h-screen">
+      <div className="w-full md:max-w-2xl border-x border-slate-400">
+        <div className="border-b border-slate-400 p-4 flex">
+          <SignedOut>
+            <div className="flex justify-center">
+              <SignInButton />
+            </div>
+          </SignedOut>
+          <SignedIn>
+            <div className="flex justify-center">
+              <SignOutButton />
+            </div>
+          </SignedIn>
+        </div>
+        <div className="flex flex-col">
+          {data?.map((post) => (
+            <div key={post.id} className="p-8 border-b border-slate-400">{post.content}</div>
+          ))}
+        </div>
       </div>
     </div>
   );
