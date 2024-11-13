0e62735d0b2ae2efd9cd121652065bccdf0a2520
FlackCode <fl4ckc0de@gmail.com>
2024-11-13 19:01:14 +0200

    Added rate limiting with upstash

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,6 +1,8 @@
 import { clerkClient, type User } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
 
 import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 
@@ -11,6 +13,14 @@ const filterUserForClient = (user: User) => {
     imageUrl: user.imageUrl
   }
 }
+
+
+// 3 requests per 1 min
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true
+})
 
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async({ ctx }) => {
@@ -39,6 +49,10 @@ export const postsRouter = createTRPCRouter({
   }),
   create: privateProcedure.input(z.string().emoji().min(1).max(280)).mutation(async ({ctx, input}) => {
     const authorId = ctx.currentUser.id;
+
+    const {success} = await ratelimit.limit(authorId);
+
+    if (!success) throw new TRPCError({code: "TOO_MANY_REQUESTS"});
 
     const post = await ctx.db.post.create({
       data: {
