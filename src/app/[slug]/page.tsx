a107030ce1a7f69b9daacb7222b1c054ddc66285
FlackCode <fl4ckc0de@gmail.com>
2024-11-16 09:34:06 +0200

    Profile page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import Image from "next/image"
 import { LoadingPage } from "~/components/loading"
 import { api } from "~/trpc/react"
 
@@ -10,8 +11,16 @@ export default function ProfilePage() {
     if (!data) return <div>404</div>
 
     return (
-        <div className="border-b border-slate-400 p-4 flex">
-                Hello {data.username}
+        <div className="h-36 bg-slate-700 relative">
+                <Image 
+                src={data.imageUrl}
+                alt={`${data.username}'s profile picture`}
+                width={128}
+                height={128}
+                className="-mb-16 absolute bottom-0 left-0 ml-4 border-4 border-black rounded-full"/>
+                <div className="h-48"></div>
+                <div className="p-4 text-2xl font-bold">{`@${data.username}`}</div>
+                <div className="border-b border-slate-400 w-full"></div>
         </div>
     )
 }
